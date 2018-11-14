e7fedc3a340a8e5a8fd901c93569a08a823570c4
Alexander Andreev <al101an@mail.ru>
2018-11-14 23:29:17 +0700

    Implement mkdir, rmdir, download

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import wsgiref.util
 from wsgiref.simple_server import make_server
 import json
 import os
@@ -14,36 +13,56 @@ def file_server(environ, start_response):
     path = environ['PATH_INFO']
     query = environ['QUERY_STRING']
 
-    print(path)
-    print(query)
+    fullpath = BASE_ROOT_PATH + path
 
     response = {'path': path, 'query': query}
 
     if query == "ls":
         try:
-            fullpath = BASE_ROOT_PATH + path
-            print(fullpath)
             file_folder_list = os.listdir(fullpath)
-            response['result'] = file_folder_list
         except FileNotFoundError:
-            print("Wrong path")
-            response['error'] = "Wrong path"
+            response['error'] = "File Not Found"
+        except Exception as e:
+            response['error'] = str(e)
+        else:
+            response['result'] = file_folder_list
 
     elif query == "download":
-        response['error'] = "Not implement!"
-        pass
+        data = b''
+        try:
+            with open(fullpath, 'rb', buffering=0) as f:
+                data = f.readall()
+        except FileNotFoundError:
+            response['error'] = "File Not Found"
+        except Exception as e:
+            response['error'] = str(e)
+        else:
+            return [data]
 
     elif query == "mkdir":
-        response['error'] = "Not implement!"
-        pass
+        try:
+            os.mkdir(fullpath)
+        except FileNotFoundError:
+            response['error'] = "File Not Found"
+        except Exception as e:
+            response['error'] = str(e)
+        else:
+            response['result'] = "Success! Folder was created"
 
     elif query == "rmdir":
-        response['error'] = "Not implement!"
-        pass
+        try:
+            os.rmdir(fullpath)
+        except FileNotFoundError:
+            response['error'] = "File Not Found"
+        except Exception as e:
+            response['error'] = str(e)
+        else:
+            response['result'] = "Success! Folder was removed"
+    else:
+        response['error'] = "Not Correct Query"
 
     json_string = json.dumps(response, indent=4)
     bytes = json_string.encode('ascii')
-    print(bytes)
     return [bytes]
 
 
