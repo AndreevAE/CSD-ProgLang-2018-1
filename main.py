a5957bf5c2b425580873561e69277192841c36bc
Alexander Andreev <al101an@mail.ru>
2018-11-14 22:50:36 +0700

    Add description to README Implement file and folders listing method, server with json API

--- main.py
+++ main.py
@@ -1,23 +1,55 @@
-"""
-Задания 1 и 2 выполняются совместно и должны иметь согласованные протоколы.
+import wsgiref.util
+from wsgiref.simple_server import make_server
+import json
+import os
 
-Задание 1 - написать на языке Python веб приложение реализующее json API методы
-для доступа к файловой системе.
+BASE_ROOT_PATH = "./temp_file_folder"
 
-Необходимо реализовать веб приложение с использованием стандартного
-BaseHTTPServer или wsgiref, в т.ч. допускается решения с использованием
-программных каркасов Flask, Django, web2py, bottle.
+def file_server(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-type', 'application/json')]
+    start_response(status, headers)
 
-Требуется реализовать методы перечисления файлов и папок по передаваемому
-пути, удаленное скачивание файлов, создание и удаление пустых папок. По деланию
-список может быть расширен заливкой/удалением файлов и т.п..
+    method = environ['REQUEST_METHOD']
+    path = environ['PATH_INFO']
+    query = environ['QUERY_STRING']
 
-В настройках скрипта должна быть корневая директория, ограничивающая все
-запросы клиента на данные директорий и папок вне ее поддерева.
+    print(path)
+    print(query)
 
-По url / должен отдаваться код клиентского js приложения из здания 2 (можно
-добавить позже, при сдаче 2 лабы). Для успешной сдачи необходимо предоставить
-описание протокола и примеры url для тестирования всех методов из браузера.
+    response = {'path': path, 'query': query}
 
-Срок выполнения 1 задания - 15 ноября
-"""
+    if query == "ls":
+        try:
+            fullpath = BASE_ROOT_PATH + path
+            print(fullpath)
+            file_folder_list = os.listdir(fullpath)
+            response['result'] = file_folder_list
+        except FileNotFoundError:
+            print("Wrong path")
+            response['error'] = "Wrong path"
+
+    elif query == "download":
+        response['error'] = "Not implement!"
+        pass
+
+    elif query == "mkdir":
+        response['error'] = "Not implement!"
+        pass
+
+    elif query == "rmdir":
+        response['error'] = "Not implement!"
+        pass
+
+    json_string = json.dumps(response, indent=4)
+    bytes = json_string.encode('ascii')
+    print(bytes)
+    return [bytes]
+
+
+if __name__ == "__main__":
+    print("Starting wsgiref server")
+    with make_server('', 8000, file_server) as httpd:
+        print("Serving on port 8000...")
+
+        httpd.serve_forever()
