bf7fe494bb0807bb51a678a614e2bfc139613fd9
Alexander Andreev <al101an@mail.ru>
2018-11-28 21:52:45 +0700

    add starting javascript frontend wrapper for backend api, add try-except for starting server and handling KeyboardInterrupt

--- main.py
+++ main.py
@@ -6,8 +6,121 @@ BASE_ROOT_PATH = "./temp_file_folder"
 
 def file_server(environ, start_response):
     status = '200 OK'
-    headers = [('Content-type', 'application/json')]
+    headers = [('Content-type', 'text/html; charset=utf-8')]
     start_response(status, headers)
+
+    html1 = [b"""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>File Server</title>
+    <style>
+        th, td, p, input {
+            font:14px Verdana;
+        }
+        table, th, td
+        {
+            border: solid 1px #DDD;
+            border-collapse: collapse;
+            padding: 2px 3px;
+            text-align: center;
+        }
+        th {
+            font-weight:bold;
+        }
+    </style>
+</head>
+<body>
+    <p id="showData"></p>
+</body>
+
+<script>
+    function httpGet(theUrl)
+    {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+        xmlHttp.send( null );
+        return xmlHttp.responseText;
+    }
+
+    function rootPath()
+    {
+        jsonString = httpGet("/?ls");
+        json = JSON.parse(jsonString);
+        createExplorerFromJSON(json);
+    }
+
+    function createExplorerFromJSON(json)
+    {
+        var table = document.createElement("table");
+
+        for (var key in json) {
+            switch (key) {
+                case 'path':
+                    console.log(key + ':' + json[key]);
+                    // TODO: to header
+                    break;
+                case 'query':
+                    console.log(key + ':' + json[key]);
+                    // TODO: not show
+                    break;
+                case 'error':
+                    console.log(key + ':' + json[key]);
+                    break;
+                case 'result':
+                    console.log(key + ':' + json[key]);
+
+                    break;
+                default:
+                    console.log(key + ':' + json[key]);
+                    break;
+            }
+        }
+
+        var rows = [];
+        for (var key in json) {
+            if (rows.indexOf(key) === -1) {
+                rows.push(key);
+            }
+        }
+
+        for (var i = 0; i < rows.length; i++) {
+            var tr = table.insertRow(-1);
+            var th = document.createElement("th");
+            th.innerHTML = rows[i];
+            tr.appendChild(th);
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = json[rows[i]];
+        }
+
+        var divContainer = document.getElementById("showData");
+        divContainer.innerHTML = "";
+        divContainer.appendChild(table);
+
+        var dirNameTextField = document.createElement("input");
+        dirNameTextField.setAttribute('type', 'text');
+        dirNameTextField.setAttribute('id', 'dirNameTextField');
+        divContainer.appendChild(dirNameTextField)
+
+        var mkdirButton = document.createElement("input");
+        mkdirButton.setAttribute('type', 'button');
+        mkdirButton.setAttribute('value', 'Make New Dir');
+        var createDirHandler = function() {
+            return function() {
+                var dirName = document.getElementById('dirNameTextField').value;
+                httpGet(dirName + "?mkdir");
+                rootPath();
+            };
+        };
+        mkdirButton.onclick = createDirHandler();
+        divContainer.appendChild(mkdirButton);
+    }
+
+    rootPath();
+</script>
+
+</html>
+"""]
 
     method = environ['REQUEST_METHOD']
     path = environ['PATH_INFO']
@@ -19,7 +132,7 @@ def file_server(environ, start_response):
 
     if not os.path.exists(BASE_ROOT_PATH):
         os.makedirs(BASE_ROOT_PATH)
-    
+
     if query == "ls":
         try:
             file_folder_list = os.listdir(fullpath)
@@ -62,7 +175,10 @@ def file_server(environ, start_response):
         else:
             response['result'] = "Success! Folder was removed"
     else:
-        response['error'] = "Not Correct Query"
+        if path == "/":
+            return html1
+        else:
+            response['error'] = "Not Correct Query"
 
     json_string = json.dumps(response, indent=4)
     bytes = json_string.encode('ascii')
@@ -73,5 +189,9 @@ if __name__ == "__main__":
     print("Starting wsgiref server")
     with make_server('', 8000, file_server) as httpd:
         print("Serving on port 8000...")
-
-        httpd.serve_forever()
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("Stop serving")
+        except Exception as e:
+            print(str(e))
